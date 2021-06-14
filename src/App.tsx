d4a3f63bc467f75684991477c4252550eaebdcdb
Yiğithan Yücedağ <yigithanyucedag@gmail.com>
2021-06-15 02:55:06 +0300

    font menu init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import IncrementDecrement from "./components/IncrementDecrement";
 
 function App() {
+  useEffect(() => {}, []);
+
+  const [fontSize, setFontSize] = useState(15);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <div className="min-h-screen bg-gray-900 w-2/3 md:w-80 p-3">
+        <div className="menu-container">
+          <label className="menu-title">FONT</label>
+          <div className="menu-items">
+            <div className="menu-item">
+              <span className="menu-item-title">Font Size</span>
+              <div className="menu-item-container">
+                <span className="text-sm text-gray-400">18pt</span>
+                <IncrementDecrement
+                  onIncrement={() => console.log("increment font size")}
+                  onDecrement={() => console.log("decrement font size")}
+                  incrementDisabled={false}
+                  decrementDisabled={false}
+                />
+              </div>
+            </div>
+            <div className="menu-item">
+              <span className="menu-item-title">Line Spacing</span>
+              <div className="menu-item-container">
+                <span className="text-sm text-gray-400">2pt</span>
+                <IncrementDecrement
+                  onIncrement={() => console.log("increment line spacing")}
+                  onDecrement={() => console.log("decrement line spacing")}
+                  incrementDisabled={false}
+                  decrementDisabled={false}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-600 min-h-screen flex-1"></div>
     </div>
   );
 }
