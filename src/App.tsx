51bb6ed8f62c2bccef451eb901540e5a2c20b9d6
Yiğithan Yücedağ <yigithanyucedag@gmail.com>
2021-06-16 08:16:03 +0300

    warnings fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import IncrementDecrement from "./components/IncrementDecrement";
 import Editor from "react-simple-code-editor";
 import * as prism from "prismjs";
@@ -32,8 +32,6 @@ import useScreenshot from "./hooks/useScreenshot";
 // TODO: make the sidebar responsive
 
 function App() {
-  useEffect(() => {}, []);
-
   const [selectedTheme, setSelectedTheme] = useState(THEMES[0]);
 
   const [selectedLanguage, setSelectedLanguage] = useState(LANGUAGES[0]);
@@ -46,6 +44,7 @@ function App() {
   const [fixedWidth, setFixedWidth] = useState(true);
   const [codeWidth, setCodeWidth] = useState(700);
 
+  /* eslint-disable no-template-curly-in-string */
   const [code, setCode] = useState(
     "function $initHighlight(block, cls) {\n  try {\n   if (cls.search(/\bno-highlight\b/) != -1)\n     return process(block, true, 0x0F) + ` class='${cls}'`;\n  } catch (e) {\n   /* handle exception */\n  }\n  for (var i = 0 / 2; i < classes.length; i++) {\n   if (checkCondition(classes[i]) === undefined)\n     console.log('undefined');\n   }\n}\n\nexport  $initHighlight;"
   );
@@ -185,6 +184,7 @@ function App() {
             <a
               className="flex items-center gap-x-2"
               href="https://github.com/yigithanyucedag/kodsnap"
+              rel="noreferrer"
               target="_blank"
             >
               <svg
@@ -196,7 +196,10 @@ function App() {
               >
                 <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
               </svg>
-              <img src="https://img.shields.io/github/stars/yigithanyucedag/kodsnap.svg?style=social&label=Star&maxAge=2592000" />
+              <img
+                alt="GitHub"
+                src="https://img.shields.io/github/stars/yigithanyucedag/kodsnap.svg?style=social&label=Star&maxAge=2592000"
+              />
             </a>
             <button
               onClick={generateImage}
